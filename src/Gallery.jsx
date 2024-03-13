3fc06c64f30eeda7cadf228658da42796013a316
thornicek <tomas.hornicek8@email.cz>
2024-03-13 17:21:22 -0400

    (Dark Mode finished)

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,8 +1,8 @@
 import { useGlobalContext } from "./context"
 
 const Gallery = () => {
-    const greeting = useGlobalContext()
-    console.log(greeting)
+   
+  
   return (
     <h2>Gallery</h2>
   )
--- src/context.jsx
+++ src/context.jsx
@@ -3,9 +3,16 @@ import {createContext, useContext, useState, useEffect} from "react"
 const AppContext = createContext();
 
 export const AppProvider = ({children}) => {
-    const greeting = 'hello'
+    const[isDarkTheme, setIsDarkTheme] = useState(false)
 
-    return <AppContext.Provider value={{greeting}}>
+    const toggleDarkTheme = () => {
+        const newDarkTheme = !isDarkTheme
+        setIsDarkTheme(newDarkTheme)
+        const body = document.querySelector('body')
+        body.classList.toggle('dark-theme', newDarkTheme)
+    }
+
+    return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme}}>
         {children}
     </AppContext.Provider>
 }
