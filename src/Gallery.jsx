c7c479b21e5dd1fc45348a9dfdb21b1257fdbaf6
thornicek <tomas.hornicek8@email.cz>
2024-03-15 13:15:25 -0400

    Project Finished

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,14 +1,16 @@
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
+import { useGlobalContext } from "./context"
 
 const url =
-  'https://api.unsplash.com/search/photos?client_id=WJeEoRCd0w-03k8fy9h8UBN7WcElwLnWVGfMV2IvaGQ&query=office'
+  `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}`
 
 const Gallery = () => {
+    const {searchTerm} = useGlobalContext()
    const response = useQuery({
-    queryKey:['images'],
+    queryKey:['images', searchTerm],
     queryFn:async() => {
-      const result = await axios.get(url)
+      const result = await axios.get(`${url}&query=${searchTerm}`)
 
       return result.data
     }
--- src/context.jsx
+++ src/context.jsx
@@ -2,17 +2,35 @@ import {createContext, useContext, useState, useEffect} from "react"
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches
+  const storedDarkMode = localStorage.getItem('darkTheme')
+
+  if (storedDarkMode === null) {
+    return prefersDarkMode
+  }
+
+  return storedDarkMode === 'true'
+}
+
 export const AppProvider = ({children}) => {
-    const[isDarkTheme, setIsDarkTheme] = useState(false)
+    const[isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode())
+    const[searchTerm, setSearchTerm] = useState('cat')
 
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme
         setIsDarkTheme(newDarkTheme)
-        const body = document.querySelector('body')
-        body.classList.toggle('dark-theme', newDarkTheme)
+        localStorage.setItem('darkTheme')
     }
 
-    return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme}}>
+    useEffect(()=>{
+        document.body.classList.toggle('dark-theme', isDarkTheme)
+    },[isDarkTheme])
+
+    return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme,
+     searchTerm, setSearchTerm}}>
         {children}
     </AppContext.Provider>
 }
